e8fc801dd1b4402be67c370f8cc0484fa60922f1
Rizwan Hawwari <rzwnhawari@gmail.com>
2024-10-06 15:54:55 +0700

    All Work

--- produk_keluar.php
+++ produk_keluar.php
@@ -25,7 +25,7 @@ if (!$result) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Produk Keluar</title>
-  <link rel="stylesheet" href="css/produk_keluar.css">
+  <link rel="stylesheet" href="css/produkkeluar.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
--- produk_masuk.php
+++ produk_masuk.php
@@ -28,7 +28,7 @@ if (!$result) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Produk Masuk</title>
-  <link rel="stylesheet" href="css/produk_masuk.css">
+  <link rel="stylesheet" href="css/produkmasuk.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
