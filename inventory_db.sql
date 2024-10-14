23b6e03b7c8cbb3cfa776724888fd7fd5fbc4b00
Rizwan Hawwari <rzwnhawari@gmail.com>
2024-10-14 17:56:19 +0700

    Renew sql

--- inventory_db.sql
+++ inventory_db.sql
@@ -29,9 +29,13 @@ CREATE TABLE `produk` (
   `produk_masuk` int(11) DEFAULT 0,
   `produk_keluar` int(11) DEFAULT 0,
   `jumlah` int(11) DEFAULT 0,
+  `penerima_barang` varchar(255) DEFAULT NULL,
+  `penerima_barang_keluar` varchar(255) DEFAULT NULL,
+  `alasan_keluar` enum('Terjual','Expired') DEFAULT NULL,
+  `waktu` time DEFAULT NULL,
   `tanggal` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +44,7 @@ CREATE TABLE `produk` (
 
 LOCK TABLES `produk` WRITE;
 /*!40000 ALTER TABLE `produk` DISABLE KEYS */;
-INSERT INTO `produk` VALUES (22,'Air Mineral','Le Mineral',45,32,45,'2024-10-11'),(25,'Air Mineral','Crystaline',56,10,56,'2024-10-11'),(26,'Air Mineral','Oasis',59,10,59,'2024-10-11'),(27,'Makanan','Potato Chip',100,0,100,'2024-10-11'),(28,'Makanan','Roma Kelapa',50,0,50,'2024-10-11');
+INSERT INTO `produk` VALUES (29,'Air Mineral','Aqua',120,30,79,'Ada','Maliki','Terjual','17:42:00','2024-10-14'),(30,'Makanan','Sushi Bako',68,10,66,'Mas Hardin Sukamto','Zakran','Expired','17:44:00','2024-10-14'),(31,'Xx','ada',12,0,12,'adas',NULL,NULL,'00:00:00','2024-10-14');
 /*!40000 ALTER TABLE `produk` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-11 23:09:25
+-- Dump completed on 2024-10-14 17:55:40
